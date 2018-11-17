b4c265573b24a8ec576e4f7728cb2777675d8446
Marques Develop <charlesdevelop@hotmail.com>
2018-11-17 11:52:18 -0200

    commit inicial

---
+++ app/src/main/java/com/bioreciclagem/alder/bioreciclagem/Cadastro_Usuario_MainActivity.java
@@ -0,0 +1,13 @@
+package com.bioreciclagem.alder.bioreciclagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_Usuario_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario_main);
+    }
+}
---
+++ app/src/main/java/com/bioreciclagem/alder/bioreciclagem/Login_MainActivity.java
@@ -0,0 +1,13 @@
+package com.bioreciclagem.alder.bioreciclagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+    }
+}
